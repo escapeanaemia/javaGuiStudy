860dac996160f1445d56a65e746564c86786a399
Andrew <hsj3171@naver.com>
2018-05-17 01:39:46 +0900

    TextArea 입력후 파일 저장 버튼을 누르면 계층 분류 후 JSON 파일로 저장.

--- JavaGuiPractice/src/GuiClass/TestUI.java
+++ JavaGuiPractice/src/GuiClass/TestUI.java
@@ -1,8 +1,11 @@
 package GuiClass;
 import java.awt.Color;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -15,8 +18,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
-
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+//import java.util.Map;
 public class TestUI extends JFrame {
 //	JTextField BoxInfo;
 	JTextArea BoxArea;
@@ -77,18 +81,70 @@ public class TestUI extends JFrame {
 				System.out.println("splitedInfo["+i+"] : "+splitedInfo[i]);
 				
 			}
+			
+//			//최종JSON객체 선언, (JSONOBject 선언)
+//			JSONObject jsonObject = new JSONObject();
+//			//JSON정보를 저장할 Array 생성
+//			JSONArray splitedTextArrayJS = new JSONArray();
+//			//잘라진 문자열 정보가 들어갈 JSONObject 선언
+//			JSONObject splitedTextInfoJS = new JSONObject();
+//			
+			
+			JSONObject jsonObject = new JSONObject();
+			JSONArray jsonArray = new JSONArray();
+			JSONArray jsonArray2 = new JSONArray();
+			JSONArray firstDataArrayJS = new JSONArray();
+			JSONArray secondDataArrayJS = new JSONArray();
+			JSONObject thirdDataObjectJS = new JSONObject();
+			
+			String oneSpacedString = "";
+			int j = 0;
 			for(int i = 0 ; i< IndexOfSplitedInfo;i++ ){
 				String	TemporarySplitedInfo = splitedInfo[i];
-				if(TemporarySplitedInfo.substring(0,1) == "\t"){
+				
+				if(TemporarySplitedInfo.charAt(0) == ' '){
 					//띄어쓰기 하나 판별
-					if(TemporarySplitedInfo.substring(1,2)=="\t"){
-						//띄어쓰기 두개 판별 
-					}else{
+					
+					if(TemporarySplitedInfo.charAt(1)==' '){
 						
+						//띄어쓰기 두개 판별 
+						System.out.println("띄어쓰기 두개가 감지됨");
+						jsonObject.put(oneSpacedString + " "+ j, TemporarySplitedInfo);
+//						jsonArray.add(j, TemporarySplitedInfo);
+						
+						j++;
+				
+					}else{
+						//띄어쓰기 하나만 했을 경우의 잘린 문자를 저장한다. 그리고 여기에다 노드를 붙여주면 된다.
+//						jsonObject.put(oneSpacedString, jsonArray);
+						oneSpacedString = splitedInfo[i];
+						System.out.println("띄어쓰기 하나가 감지됨");
+//						jsonArray.clear();
+						j=0;
 					}
-				}else{
+//					jsonArray2.add(jsonObject);
 					
 				}
+			}
+			JSONObject finalJsonObject = new JSONObject();
+			finalJsonObject.put(splitedInfo[0], jsonObject);
+			
+			System.out.println(finalJsonObject);
+			
+			//파일 만들기 
+			//파일 이름
+			String fileName = splitedInfo[0]+".json";
+			
+			try{
+				File file = new File(fileName);
+				FileWriter fw = new FileWriter(file, true);
+				
+				fw.write(finalJsonObject.toString());
+				fw.flush();
+				
+				fw.close();
+			}catch(Exception e){
+				e.printStackTrace();
 			}
 		}
 	}
